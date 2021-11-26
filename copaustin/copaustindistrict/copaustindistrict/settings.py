ecdde625d0f015dadfec4fd1096d5b8644cd36db
aurfkwar <81349933+aurfkwar@users.noreply.github.com>
2021-11-26 03:47:48 -0600

    Update settings.py

--- copaustin/copaustindistrict/copaustindistrict/settings.py
+++ copaustin/copaustindistrict/copaustindistrict/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hmz(vxu86q8in2&b3u1dqzou8-uqlzjuaxbqucf&86e50r27$g'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
